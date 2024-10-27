22914e5536f7ba36aeebc44f82c62f63e450c56c
Gregory Edison <gregory.edison1993@gmail.com>
2024-10-27 16:47:16 +0100

    write PONG

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,14 @@
-use std::net::TcpListener;
 use eyre::Result;
+use std::io::Write;
+use std::net::TcpListener;
 
 fn main() -> Result<()> {
     let listener = TcpListener::bind("127.0.0.1:6379")?;
 
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
-                println!("accepted new connection");
+            Ok(mut stream) => {
+                stream.write_all(b"+PONG\r\n")?;
             }
             Err(e) => {
                 println!("error: {}", e);
