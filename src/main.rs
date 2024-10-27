8d01ee7ce976df78c8348c697da8fef5289eb779
Gregory Edison <gregory.edison1993@gmail.com>
2024-10-27 16:25:52 +0100

    first task

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,19 @@
-#![allow(unused_imports)]
 use std::net::TcpListener;
+use eyre::Result;
 
-fn main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    println!("Logs from your program will appear here!");
+fn main() -> Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:6379")?;
 
-    // Uncomment this block to pass the first stage
-    //
-    // let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
-    //
-    // for stream in listener.incoming() {
-    //     match stream {
-    //         Ok(_stream) => {
-    //             println!("accepted new connection");
-    //         }
-    //         Err(e) => {
-    //             println!("error: {}", e);
-    //         }
-    //     }
-    // }
+    for stream in listener.incoming() {
+        match stream {
+            Ok(_stream) => {
+                println!("accepted new connection");
+            }
+            Err(e) => {
+                println!("error: {}", e);
+            }
+        }
+    }
+
+    Ok(())
 }
